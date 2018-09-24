bc813db7446def47be44abb92f73c3ee879b4e6d
Tyler Hoban <admin@tylerhoban.com>
2018-09-24 11:19:01 -0400

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function googleAnalyticsMiddleware(data) {
 }
 
 var unblockerConfig = {
-    prefix: '/proxy/',
+    prefix: '/p/',
     responseMiddleware: [
         googleAnalyticsMiddleware
     ]
