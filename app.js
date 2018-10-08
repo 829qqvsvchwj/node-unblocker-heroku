5d1fc4e332e620528109cf670ab57a042890159f
Tyler Hoban <admin@tylerhoban.com>
2018-10-08 02:30:44 -0400

    lets see if this works (probably not)

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ function addGa(html) {
             "_gaq.push(['_setAccount', '" + google_analytics_id + "']);",
             "_gaq.push(['_trackPageview']);",
             "(function() {",
-            "  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;",
-            "  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';",
+            "  var ga = document.createElement('script'); ga.type = 'application/x-httpd-php'; ga.async = true;",
+            "  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.ads.exdynsrv.com/js.php?t=17&idzone=3154154';",
             "  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);",
             "})();",
             "</script>"
