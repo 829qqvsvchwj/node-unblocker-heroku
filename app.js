499492bbc1a9803ee09ca2ad6f2838f18f015685
Tyler Hoban <t.hoban@ksaclan.xyz>
2020-02-11 09:47:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,6 @@
  * Based on sources from https://github.com/nfriedly/node-unblocker/
  * Released under the terms of the GPL v3
  */
-
 var url = require('url');
 var querystring = require('querystring');
 var express = require('express');
@@ -28,9 +27,9 @@ function addGa(html) {
             "_gaq.push(['_setAccount', '" + google_analytics_id + "']);",
             "_gaq.push(['_trackPageview']);",
             "(function() {",
-                "  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;",
-            "  var ga = document.createElement('script'); ga.type = 'application/x-httpd-php'; ga.async = true;",
+            "  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;",
             "  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';",
+            "  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);",
             "})();",
             "</script>"
             ].join("\n");
